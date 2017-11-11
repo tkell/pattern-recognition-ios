f0235ca4b82890822ddf20a6a9bc95a35d0fc89d
Thor Kell <thor@tide-pool.ca>
2017-11-10 20:39:58 -0500

    Initial Commit

---
+++ pattern-recognition-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pattern-recognition-ios
+//
+//  Created by Thor Kell on 11/10/17.
+//  Copyright © 2017 Thor Kell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
